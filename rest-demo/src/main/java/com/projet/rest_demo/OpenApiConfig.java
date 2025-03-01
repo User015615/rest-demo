62a782efbaf3edb947c291fae0e37aa2a83a1899
User015615 <remi.yacoub6@gmail.com>
2025-03-01 11:09:54 +0100

    SWAGGER UI

---
+++ rest-demo/src/main/java/com/projet/rest_demo/OpenApiConfig.java
@@ -0,0 +1,17 @@
+package com.projet.rest_demo;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+            .info(new Info()
+                .title("API Documentation")
+                .version("3.0.0")
+                .description("API endpoints"));
+    }
+}
